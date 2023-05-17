6121547e46ec3b909efdc4d5b7b5381ad4bdfd4a
[Elijah <[efrancis64@gmail.com]>
2023-05-17 18:17:56 -0400

    removed merged generatePassword into writePassword, fixed writePassword

--- script.js
+++ script.js
@@ -3,67 +3,59 @@ var uppercaseChar = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S",
 var numberChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "?", "+", "=", "|", "<", ">"];
 var possibleChar = [];
-var length = 0;
+var passwordLength = 0;
+var password = "";
 
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function getOptions() {
-  var length = parseInt(
-    prompt("How many characters would you like your password to contain"),
-    10
-  );
+  possibleChar = [];
 
+  // Asking for length
+  passwordLength = parseInt(
+    prompt("How many characters would you like your password to contain?"),
+    10
+    );
 
   // Checks that password length is valid
-  if (Number.isNaN(length)) {
+  if (Number.isNaN(passwordLength)) {
     alert("Password length must be a number between 8 and 128");
-    return null;
-  } else if (length < 8) {
+    return false;
+  } else if (passwordLength < 8) {
     alert("Password length must be a number between 8 and 128");
-    return null;
-  } else if (length > 128) {
+    return false;
+  } else if (passwordLength > 128) {
     alert("Password length must be a number between 8 and 128");
-    return null;
+    return false;
   }
 
   // Asks for password criteria
   if (confirm("Include lowercase characters")) {
   possibleChar = possibleChar.concat(lowercaseChar);
   }
-
   if (confirm("Include uppercase characters")) {
     possibleChar = possibleChar.concat(uppercaseChar);
   }
-
   if(confirm("Include numbers")) {
     possibleChar = possibleChar.concat(numberChar);
   }
-
   if(confirm("Include special characters")) {
     possibleChar = possibleChar.concat(specialChar);
   }
-
-}
-
-function generatePassword() {
-  var options = getOptions();
-
-  for(var i = 0; i < length; i++) {
-    var randomIndex = Math.floor(Math.random() * arr.length);
-    possibleChar[randomIndex];
-  }
-
-  if (!options) return null;
+  return true;
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  if (getOptions()) {
+    password = "";
+    for(var i = 0; i < passwordLength; i++) {
+      var randIndex = Math.floor(Math.random() * possibleChar.length);
+      password = password + possibleChar[randIndex];
+    };
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  };
 }
 
 // Add event listener to generate button
